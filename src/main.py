1c88131aacc48b81e77aea75cd6fd1139f211360
Executed419 <lucker10@interia.pl>
2023-11-16 10:44:22 +0100

    Signal generators add hard-coded amplitude to noise and writes it to file

--- src/main.py
+++ src/main.py
@@ -2,14 +2,20 @@ from NoiseGenerator import NoiseGenerator
 from SignalGenerator import SignalGenerator
 
 SIGMA = 1
-SAMPLE_COUNT = 10
+SAMPLE_COUNT = 2048
+LINE_COUNT = 10
 NOISE_FILE_PATH = "../data/noise.bin"
+SIGNAL_FILE_PATH = "../data/signal.bin"
 
 if __name__ == '__main__':
-    noise = NoiseGenerator(2048, 1, NOISE_FILE_PATH)
-    signal = SignalGenerator(2048, NOISE_FILE_PATH)
+    noise = NoiseGenerator(SAMPLE_COUNT, 1, NOISE_FILE_PATH)
+    signal = SignalGenerator(SAMPLE_COUNT, NOISE_FILE_PATH, 6, LINE_COUNT,SIGNAL_FILE_PATH)
 
-    noise.append_noise_to_bin(10)
+    noise.append_noise_to_bin(LINE_COUNT)
     print("")
-    print(signal.read_noise_from_bin(0, 10))
+    noise_list = signal.read_noise_from_bin(0, LINE_COUNT)
+    noise_and_signal = signal.append_signal_to_noise(noise_list)
+    signal.append_signal_to_bin(noise_and_signal)
     noise.clear_noise_bin()
+    signal.clear_signal_bin()
+
