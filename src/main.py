65be352a5f6f0c2d7cd92b6efd3b23fea9d7ad1b
laptopKhasar <lucker10@interia.pl>
2023-11-21 12:00:37 +0100

    SignalGenerator now writes signal index to file as list; added SignalProperties that can read it and store for CFAR use

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import json
 
 from NoiseGenerator import NoiseGenerator
+from SignalProperties import SignalProperties
 from SignalGenerator import SignalGenerator
 from BinFileManager import BinFileManager
 
@@ -11,7 +12,7 @@ if __name__ == '__main__':
     locals().update(dict)
 
     noise = NoiseGenerator(SAMPLE_COUNT, SIGMA, NOISE_FILE_PATH)
-    signal = SignalGenerator(DB, SIGMA)
+    signal = SignalGenerator(DB, SIGMA, SIGNAL_INDEX_PATH)
     noiseFileManager = BinFileManager(NOISE_FILE_PATH)
     signalFileManager = BinFileManager(SIGNAL_FILE_PATH)
 
@@ -25,13 +26,13 @@ if __name__ == '__main__':
 
     noise_list = noiseFileManager.read_file(0, LINE_COUNT)
 
-
-
     noise_and_signal, index_line_list = signal.append_signal_to_noise(noise_list)
-    with open(SIGNAL_INDEX_PATH, "a") as file:
-        for i in index_line_list:
-            json.dump(i, file)
+    strings = [str(x)+',' for x in index_line_list]
 
+    sp = SignalProperties(SIGNAL_INDEX_PATH)
+    sp.read_indexes_from_file()
+    x = sp.index[0]
+    x1 = sp.index[1]
     signalFileManager.append_to_file(noise_and_signal)
 
     noiseFileManager.clear_file()
