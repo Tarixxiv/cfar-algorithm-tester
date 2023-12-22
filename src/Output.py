7c992689b8fa9655c97d3a0e5a745da527f95c05
Kacper Kałużny <s188853@o365.student.pg.edu.pl>
2023-12-22 21:22:36 +0100

    New version of class FinalOutput including probability of detection and false alarm

--- src/Output.py
+++ src/Output.py
@@ -52,3 +52,45 @@ class FinalOutput:
             self.undetected_objects += len(self.input_signal_properties.index) - object_number
         elif object_number == len(self.input_signal_properties.index):
             self.false_detects += len(self.output_from_CFAR) - record_number
+
+
+class FinalOutput2:
+    # new version of class FinalOutput
+    def __init__(self, cfar_thres_length):
+        # cfar_thres_length=self.threshold_factor_max - self.threshold_factor_min + 1     <- from class CFAR
+        self.false_detects = 0
+        self.correct_detects = 0
+        self.undetected_objects = 0
+        self.header = ["dataset_number", "false_detects", "correct_detects", "undetected_objects",
+                       "detection_probability", "false_alarm_probability"]
+        self.dataset_number = 0
+        # self.update_probabilities(self, cfar_thres_length)
+        self.pd = [0] * cfar_thres_length
+        self.pf = [0] * cfar_thres_length
+        self.loopIdx = 0
+        self.cfar_thres_length = cfar_thres_length
+
+    def export_to_csv(self, filepath=None, false_detects=0, correct_detects=0, undetected_objects=0):
+        if filepath is None:
+            filepath = "output_data_default_name.csv"
+        file_exists = os.path.exists(filepath)
+        row = [self.dataset_number, false_detects, correct_detects, undetected_objects,
+               self.pd[self.loopIdx], self.pf[self.loopIdx]]
+        self.dataset_number = self.dataset_number+1
+
+        if not file_exists:
+            with open(filepath, 'x', encoding='UTF8') as csv_file:
+                writer = csv.writer(csv_file, lineterminator='\n')
+                writer.writerow(self.header)
+                writer.writerow(row)
+        else:
+            with open(filepath, 'a', encoding='UTF8') as csv_file:
+                writer = csv.writer(csv_file, lineterminator='\n')
+                writer.writerow(row)
+
+    def calculate_probabilities(self, false_detects_count, detects_count, data_len):
+        # data_len=len(data)  <- from class CFAR
+        for tIdx in range(self.cfar_thres_length):
+            self.pd[tIdx] = ((self.loopIdx-1)*self.pd(tIdx)+detects_count/data_len)/self.loopIdx
+            self.pf[tIdx] = ((self.loopIdx-1)*self.pf(tIdx)+false_detects_count/data_len)/self.loopIdx
+        self.loopIdx = self.loopIdx+1
