b963a823a70dfb1280dc5766e06b33187ef43300
Tarixxiv <r.litwin@interia.pl>
2023-12-23 14:33:38 +0100

    remove redundant binfile code

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ import json
 from NoiseGenerator import NoiseGenerator
 from SignalProperties import SignalProperties
 from SignalGenerator import SignalGenerator
-from BinFileManager import BinFileManager
 from time import time
 from Output import FinalOutput
 
@@ -25,15 +24,11 @@ if __name__ == '__main__':
 
     noise = NoiseGenerator(SAMPLE_COUNT, SIGMA, NOISE_FILE_PATH)
     signal = SignalGenerator(DB, SIGMA, SIGNAL_INDEX_PATH)
-    noiseFileManager = BinFileManager(NOISE_FILE_PATH)
-    signalFileManager = BinFileManager(SIGNAL_FILE_PATH)
     signalProperties = SignalProperties(SIGNAL_INDEX_PATH)
     cfar = CFAR_GOCA()
     output_to_file = FinalOutput()
     start_time = time()
 
-    noiseFileManager.clear_file()
-    signalFileManager.clear_file()
     with open(OUTPUT_FILE_PATH, "w"):
         pass
     with open(SIGNAL_INDEX_PATH, 'w'):
@@ -43,11 +38,9 @@ if __name__ == '__main__':
     for i in range(LINE_COUNT):
         line = noise.generate_noise_line()
         noise_list.append(line)
-    noiseFileManager.append_to_file(noise_list)
-    # noise_list = noiseFileManager.read_file(line_cursor, line_cursor + LINE_COUNT)
+
     noise_and_signal, index_line_list = signal.append_signal_to_noise(noise_list)
-    # signalFileManager.append_to_file(noise_and_signal)
-    # signal.write_indexes_to_file(index_line_list)
+
     time_per_parameter = 80  # 2 hrs / 90 parameter values
 
     print(time() - start_time, "generation done")
