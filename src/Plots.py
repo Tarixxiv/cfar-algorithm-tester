85befab49f1ae1b0f18c914ec09fd61738527736
Kacper Kałużny <s188853@o365.student.pg.edu.pl>
2024-01-19 22:49:17 +0100

    Change axis in plots and add plots Pd(Pdf) in CreatePlots

--- src/Plots.py
+++ src/Plots.py
@@ -14,7 +14,6 @@ class CreatePlots:
 
     def read_csv(self, which_cfar=0, file_path=None):
         if file_path is None:
-            # file_path = "../output/output_data_default_name.csv
             file_path = "../output/output.csv"
         file_exists = os.path.exists(file_path)
 
@@ -23,38 +22,36 @@ class CreatePlots:
             data = list(csv.reader(file, delimiter=","))
             file.close()
 
-            # number_of_row = 0
             which_row = which_cfar
             for row in data[1:]:
-                # which_row = number_of_row % 3
                 for i in range(3):
                     self.inputs_list[which_row*3+i].append(row[i])
-                # number_of_row = which_row+1
 
             for i in range(3):
-                # self.inputs_list[i] = [round(eval(j), 3) for j in self.inputs_list[i]]
                 self.inputs_list[i+3*which_row] = [eval(j) for j in self.inputs_list[i+3*which_row]]
-                if i % 3 != 0:
-                    for j in range(len(self.inputs_list[i])):
-                        self.inputs_list[i+3*which_row][j] = self.inputs_list[i+3*which_row][j] * 100
 
     def draw_plots(self):
-        fig, axs = plt.subplots(3)
+        fig, axs = plt.subplots(3, 2)
         algorithms_names = ['CFAR CA', 'CFAR GOCA', 'CFAR SOCA']
 
         for i in range(3):
-            print(self.inputs_list[3 * i])
-            print(self.inputs_list[3 * i + 1])
-            print(self.inputs_list[3 * i + 2])
 
-            axs[i].plot(self.inputs_list[3 * i], self.inputs_list[3 * i + 1], color='green',
-                        label='probability of detection')
-            axs[i].plot(self.inputs_list[3 * i], self.inputs_list[3 * i + 2], color='red',
-                        label='probability of false detection')
-            axs[i].set_title(algorithms_names[i], fontsize=15)
-            axs[i].legend(loc="upper right", title='Probabilities in %')
-            axs[i].grid(True)
-            axs[i].set_ylim(0, 100)
+            axs[i, 0].semilogy(self.inputs_list[3 * i], self.inputs_list[3 * i + 1], color='green',
+                        label='probability of detection', nonpositive='clip')
+            axs[i, 0].semilogy(self.inputs_list[3 * i], self.inputs_list[3 * i + 2], color='red',
+                        label='probability of false detection', nonpositive='clip')
+            axs[i, 0].set_title(algorithms_names[i], fontsize=15)
+            axs[i, 0].legend(loc="lower left", title='Probabilities in %')
+            axs[i, 0].grid(True)
+            axs[i, 0].set_ylim(0.00000001, 1)
+            axs[i, 0].set_xlabel('threshold value')
+
+            axs[i, 1].plot(self.inputs_list[3 * i + 1], self.inputs_list[3 * i + 2], color='black')
+            axs[i, 1].set_title(algorithms_names[i], fontsize=15)
+            axs[i, 1].grid(True)
+            axs[i, 1].set_xlabel('Probability of false detection')
+            axs[i, 1].set_ylabel('Probability of detection')
+
         fig.tight_layout()
 
 
