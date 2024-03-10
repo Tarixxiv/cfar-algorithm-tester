bc5001fa6c9f0597f3a2749463566838b6cec5e5
Executed419 <lucker10@interia.pl>
2024-03-10 23:24:21 +0100

    rewrite to c++

--- src/Signal.cpp
+++ src/Signal.cpp
@@ -1,124 +1,71 @@
 #include <chrono>
-#include <cmath>
+#include <cMath>
 #include <fstream>
 #include <iostream>
 #include <queue>
 #include <random>
+#include <string>
 #include <thread>
 #include <vector>
-#include <algorithm>
-#include <queue>
 
 class Signal
 {
-private:
+public:
+    std::queue<int> object_index;
+    int length = 1024;
     float sigma = 1;
     float snr_dB = 12;
-    std::vector<float> signal_amplitudes = {(float)pow(10, snr_dB / 20) * sigma};
-    std::default_random_engine noise_generator = std::default_random_engine((time(nullptr)));
-
-
-    void noise_generation()
+    float* samples = NULL;
+    std::default_random_engine generator;
+    Signal()
     {
-        samples.reserve(length);
-        std::normal_distribution<float> dist(0, sigma);
-        for (int index = 0; index < length; index++)
-        {
-            samples.push_back(dist(noise_generator));
-        }
     }
-
-    static bool isIntInVector(int value, std::vector<int> vector){
-        return !(std::find(vector.begin(), vector.end(), value) == vector.end()) || vector.back() == value ;
-    }
-
-#ifdef UNIT_TESTING
-    static int generate_int_in_range(int min, int max) {
-        static size_t test_count = 0;
-        if (test_count == 0) {
-            test_count++;
-            return min;
-        }
-        if (test_count == 1) {
-            test_count++;
-            return max;
-        }
-        if (test_count >= 2) {
-            test_count++;
-            return (min + max) / 2;
-        }
-    }
-#else
-    static int generate_int_in_range(int bot, int top){
-        return rand() % (top + 1 - bot) + bot;
-    }
-#endif
-
-    void signal_generation(int signal_count)
-    {
-        int index;
-        for (int i = 0; i < signal_count; ++i) {
-            if (object_indexes.empty()){
-                index = rand() % length;
-            } else{
-                while (!
-                (index >= 0 && index < length &&
-                !(isIntInVector(index,object_indexes)))){
-                    int neighbour_index = rand() % object_indexes.size();
-                    index = object_indexes[neighbour_index] + generate_int_in_range(-signal_max_distance,
-                                                                             signal_max_distance);
-                }
-            }
-            object_indexes.push_back(index);
-            samples[index] += signal_amplitudes[i%signal_amplitudes.size()];
-        }
-
-    }
-
-    void setQueue(){
-        while (!object_indexes_queue.empty()) {
-            object_indexes_queue.pop();
-        }
-        for (const auto& element : object_indexes) {
-            object_indexes_queue.push(element);
-        }
-    }
-public:
-    std::queue<int> object_indexes_queue;
-    std::vector<int> object_indexes;
-    std::vector<float> samples;
-    int length = 2048;
-    int signal_max_distance = 10;
-
-    Signal() = default;
-
     Signal(Signal& signal)
     {
+        generator = std::default_random_engine((time(NULL)));
         this->length = signal.length;
         this->sigma = signal.sigma;
         this->snr_dB = signal.snr_dB;
-        this->signal_max_distance = signal.signal_max_distance;
-        this->signal_amplitudes = signal.signal_amplitudes;
     }
-
-    Signal(float sigma, int length, float snr_dB, int signal_max_distance, std::vector<float> signal_amplitudes)
+    Signal(float sigma, int length, float snr_dB)
     {
+        generator = std::default_random_engine((time(NULL)));
         this->length = length;
         this->sigma = sigma;
         this->snr_dB = snr_dB;
-        this->signal_max_distance = signal_max_distance;
-        this->signal_amplitudes = signal_amplitudes;
     }
-
-    void signal_and_noise_generation(int signal_count)
+    void cleanup()
     {
-        object_indexes = {};
-        samples = {};
-        srand(time(nullptr));
-        noise_generation();
-        signal_generation(signal_count);
-        setQueue();
+        delete[] samples;
+        while (!object_index.empty())
+            object_index.pop();
+    }
+    void noise_generation()
+    {
+        samples = new float[length];
+        std::normal_distribution<float> dist(0, sigma);
+        for (int index = 0; index < length; index++)
+        {
+            samples[index] = dist(generator);
+        }
+    }
+    void signal_generation()
+    {
+        float signal_from_object = pow(10, snr_dB / 20) * sigma;
+        object_index.push(rand() % length);
+        samples[object_index.front()] += signal_from_object;
     }
 
-    ~Signal() = default;
+    void signal_and_noise_generation()
+    {
+        srand(time(NULL));
+        cleanup();
+        noise_generation();
+        signal_generation();
+    }
+
+    ~Signal()
+    {
+        delete[] samples;
+    }
 };
