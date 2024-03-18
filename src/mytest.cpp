60be4051dcd5ff983e694deb7efce309099967b5
Khasar0451 <lucker10@interia.pl>
2024-03-18 17:21:10 +0100

    Test for signal distance. Tests are run with terminal with cd src; cmake -S . -B build; cmake --build build; cd build; ctest; cd ../../

--- src/mytest.cpp
+++ src/mytest.cpp
@@ -2,30 +2,36 @@
 
 #define UNIT_TESTING
 #include "Signal.cpp"
+//#include "CFAR.cpp"
 #undef UNIT_TESTING
 
 #define SIGMA 1.0f
 #define SNR_dB 12.0f
 #define DATA_LENGTH 2048
-#define TESTS_PER_THREAD 1000
-#define NUMBER_OF_THREADS 5
+#define TESTS_PER_THREAD 10
+#define NUMBER_OF_THREADS 1
 #define SIGNAL_COUNT 2
 #define CFAR_GUARD_CELLS 8
 
-// Demonstrate some basic assertions.
-//TEST(HelloTest, BasicAssertions) {
-//	// Expect two strings not to be equal.
-//	EXPECT_STRNE("hello", "world");
-//	// Expect equality.
-//	EXPECT_EQ(7 * 6, 42);
-//}
 
-TEST(CompleteTest, DetectingObjects) {
+TEST(SignalTest, SignalDistance) {
 	Signal signal(SIGMA, DATA_LENGTH, SNR_dB, CFAR_GUARD_CELLS, std::vector<float>{1,2});
-	
-	//signal.signal_and_noise_generation(SIGNAL_COUNT);
-	EXPECT_EQ(signal.generate_int_in_range(0, 6), 0);
-	EXPECT_EQ(signal.generate_int_in_range(0, 6), 6);
-	EXPECT_EQ(signal.generate_int_in_range(0, 6), 3);
+	int test_count = 3;
+	signal.signal_and_noise_generation(SIGNAL_COUNT);
 
+	for (int i = 0; i < SIGNAL_COUNT; i++) {
+        for (int i = 0; i < SIGNAL_COUNT; i++) {
+            for (int j = 0; j < SIGNAL_COUNT; j++) {
+                if (i != j) {
+					EXPECT_NE(signal.object_indexes[i], signal.object_indexes[j]);
+					EXPECT_LE(abs(signal.object_indexes[i] - signal.object_indexes[j]), signal.signal_max_distance);
+                }
+            }
+        }
+	}
 }
+//
+//TEST(CfarTest, Complete) {
+//	main();
+//	findobjects (), srand(451)
+//}
